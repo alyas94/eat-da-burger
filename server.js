4da1995094e9c685d2b280b7e65601ccff66b37c
Ariel Solano <arielsolano@Ariels-MBP.localdomain>
2018-05-03 18:29:54 -0700

    asdf

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Pull in required dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
 
 var port = process.env.PORT || 3000;
 
@@ -11,9 +10,6 @@ var app = express();
 app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
 
 // Set Handlebars as the view engine
 var exphbs = require("express-handlebars");
